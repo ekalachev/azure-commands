ebd145214cccf58e3af968b4110514e9e53c334c
Eldar Kalachev <rusgunter@gmail.com>
2020-03-15 13:25:41 +0300

    added 'Create VM script'

---
+++ create-vm.ps1
@@ -0,0 +1,29 @@
+Login-AzAccount
+
+Get-AzSubscription
+
+$subscription = Read-Host -Prompt "Enter subscription name"
+$location = Read-Host -Prompt "Enter location"
+$rgName = Read-Host -Prompt "Enter resource group name"
+$newVMName = Read-Host -Prompt "Enter a new VM name"
+$credential = Get-Credential
+
+Set-AzContext -SubscriptionName $subscription
+
+New-AzResourceGroup -Name $rgName -Location $location
+
+New-AzVm `
+    -ResourceGroupName $rgName `
+    -Name $newVMName `
+    -Location $location `
+    -VirtualNetworkName ($newVMName + "-vnet") `
+    -SubnetName ($newVMName + "-subnet") `
+    -SecurityGroupName ($newVMName + "-nsg") `
+    -PublicIpAddressName ($newVMName + "-ip") `
+    -OpenPorts 80, 3389 `
+    -Credential $credential
+
+$ipAddress = Get-AzPublicIpAddress -ResourceGroupName Iaas-ps-rg `
+| Select-Object -ExpandProperty IpAddress
+
+mstsc /v:$ipAddress
